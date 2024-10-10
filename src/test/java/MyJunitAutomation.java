8c94bb447d77e40b6471d087f473ef137aae0530
reday321 <redayk37@gmail.com>
2024-10-10 17:43:14 +0600

    After 2nd Part of Junit

--- src/test/java/MyJunitAutomation.java
+++ src/test/java/MyJunitAutomation.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import utils.Utils;
 
 import java.time.Duration;
@@ -59,6 +60,7 @@ public class MyJunitAutomation {
         actions.contextClick(buttons.get(2)).perform();
         actions.click(buttons.get(3)).perform();
     }
+
     @Test
     public void AlertHandle() throws InterruptedException {
         driver.get("https://demoqa.com/alerts");
@@ -73,6 +75,74 @@ public class MyJunitAutomation {
 
         driver.findElement(By.id("confirmButton")).click();
         driver.switchTo().alert().accept();
+
+    }
+
+    @Test
+    public void DateTimePicker() {
+        driver.get("https://demoqa.com/date-picker");
+        WebElement txtCalendarElemt = driver.findElement(By.id("datePickerMonthYearInput"));
+
+        txtCalendarElemt.sendKeys(Keys.CONTROL, "a");
+        txtCalendarElemt.sendKeys(Keys.DELETE);
+        txtCalendarElemt.sendKeys("10/10/2024");
+        txtCalendarElemt.sendKeys(Keys.ENTER);
+
+    }
+
+    @Test
+    public void OldDropDown() {
+        driver.get("https://demoqa.com/select-menu");
+
+        Select select = new Select(driver.findElement(By.id("oldSelectMenu")));
+
+        //We can do this in there ways
+
+        select.selectByIndex(1);
+        // select.selectByValue("2");
+
+        //  select.selectByVisibleText("Green");
+    }
+
+    @Test
+    public void MultiDropDown() {
+        driver.get("https://demoqa.com/select-menu");
+        Select select = new Select(driver.findElement(By.id("cars")));
+
+        if (select.isMultiple()) {
+            select.selectByVisibleText("Volvo");
+            select.selectByVisibleText("Audi");
+        }
+    }
+
+    @Test
+    public void SpecialDropDown() {
+        driver.get("https://demoqa.com/select-menu");
+
+        driver.findElement(By.className("css-1hwfws3")).click();
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ENTER).perform();
+    }
+@Test
+    public void MouseHover() {
+        driver.get("https://daffodilvarsity.edu.bd/");
+
+        List<WebElement> menu = driver.findElements(By.className("megamenu-item"));
+        Actions action = new Actions(driver);
+        action.moveToElement(menu.get(0)).perform();
+
+
+    }
+@Test
+    public void hover2(){
+       driver.get("https://www.sub.ac.bd/");
+        List<WebElement> menu = driver.findElements(By.className("drp_open"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(menu.get(0)).click().perform();
+
 
     }
 
